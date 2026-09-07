45c0546b8b4054fe22afaba3d132d2fc184e238a
tetsting19-art <tetsting19@gmail.com>
2026-09-07 16:33:03 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ضع بيانات Supabase العامة هنا فقط.
-// لا تضع service_role key في هذا الملف.
-window.SEMSEM_CONFIG = {
-  url: "https://fwmudzugbchyuqqljspf.supabase.co",
-  anonKey: "sb_publishable_xRBzbpyYPGEUZpcZFMn3Hw_krtHoepT"
-};
