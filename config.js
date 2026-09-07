3364780db7bef4b80a31b511494bde66f56aaa80
tetsting19-art <tetsting19@gmail.com>
2026-09-07 04:59:34 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// ضع بيانات Supabase العامة هنا فقط.
+// لا تضع service_role key في هذا الملف.
+window.SEMSEM_CONFIG = {
+  url: "https://fwmudzugbchyuqqljspf.supabase.co",
+  anonKey: "sb_publishable_xRBzbpyYPGEUZpcZFMn3Hw_krtHoepT"
+};
